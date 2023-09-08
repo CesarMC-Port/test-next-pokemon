e94610022f712f77907137eee3639ec3d59b1780
CesarMC-Port <morecontrol64@gmail.com>
2023-09-08 15:32:47 -0400

    new details

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -8,7 +8,7 @@ import confetti from 'canvas-confetti';
 
 import { pokeApi,httpClient } from '../../api';
 import { Layout } from '../../components/layouts';
-const {get} = require('http2-client');
+
 // import { Pokemon, PokemonListResponse } from '../../interfaces';
 // import { getPokemonInfo, localFavorites } from '../../utils';
 
@@ -34,30 +34,15 @@ const PokemonByNamePage: NextPage<Props> = ({ blog }) => {
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
-  const {data} = await get('https://api-dev.hackinghrlab.io/public/blogpost?page=1', (res:any)=>{
-      console.log(`
-        Url : 'https://api-dev.hackinghrlab.io/public/blogpost?page=1'
-        Status : ${res.statusCode}
-        HttpVersion : ${res.httpVersion}
-      `);
-    });
-
-  console.log(data)
-  // data?.blogsPosts?.map((e:any) => {
-  //   console.log("/////////////e",e)
-  //   return {
-  //       // params: {name: `${e.title.replaceAll(' ', '-').substring(0,10)}`}
-  //       params: {id: `${e.id}`}
-  //   }
-  // })
+  const {data} = await httpClient('/public/blogpost?page=1');
 
   return {
-    paths:  [
-      // String variant:
-      '/name/first-post',
-      // Object variant:
-      { params: { name: 'second-post' } },
-    ],
+    paths: data?.blogsPosts?.map((e:any) => {
+      return {
+          // params: {name: `${e.title.replaceAll(' ', '-').substring(0,10)}`}
+          params: {name: `${e.id}`}
+      }
+    }),
     fallback: false
     // fallback: 'blocking'
   }
@@ -67,13 +52,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   
-  const response = await get('https://api-dev.hackinghrlab.io/public/blogpost?page=1', (res:any)=>{
-    console.log(`
-      Url : 'https://api-dev.hackinghrlab.io/public/blogpost?page=1'
-      Status : ${res.statusCode}
-      HttpVersion : ${res.httpVersion}
-    `);
-  });
+  const response = await httpClient('/public/blogpost?page=1');
 
     const { name } = params as { name: string };
 
@@ -86,7 +65,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
     return {
         props: {
-            blog: blog ? (blog[0] || null) : null
+            name: blog ? (blog[0] || null) : null
         }
     }
 }
